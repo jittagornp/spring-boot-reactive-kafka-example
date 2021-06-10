be92c61558b5c667a22aa6312bc451ec28c5812d
jittagornpitakmetagoon <jittagornp@gmail.com>
2021-06-10 16:55:12 +0700

    RegisterConsumerImpl => RegisterConsumer

--- spring-boot-reactive-kafka-template/spring-boot-reactive-kafka-template-consumer/src/main/java/me/jittagornp/example/reactive/consumer/RegisterConsumer.java
+++ spring-boot-reactive-kafka-template/spring-boot-reactive-kafka-template-consumer/src/main/java/me/jittagornp/example/reactive/consumer/RegisterConsumer.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class RegisterConsumerImpl implements CommandLineRunner {
+public class RegisterConsumer implements CommandLineRunner {
 
     private final ReactiveKafkaConsumerTemplate<String, RegisterRequest> reactiveKafkaConsumerTemplate;
 
